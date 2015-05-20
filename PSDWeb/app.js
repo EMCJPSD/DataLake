17632c66d9af285e5facc13ff4a1874b25d2278a
wangzhizheng <wangzz2009@outlook.com>
2015-05-20 19:04:40 +0900

    convert it to express and use AJAX to upload file

---
+++ PSDWeb/app.js
@@ -0,0 +1,31 @@
+/**
+ * New node file
+ */
+var express=require('express')
+	,morgan=require('morgan')
+	,fs=require('fs');
+var app=express();
+app.use(express.static('./public'));
+app.use(morgan('dev'));
+
+app.use(express.static('./'));
+
+app.get('/', function(req,res){
+	res.sendfile('./index.html');
+});
+
+var multipart = require('connect-multiparty');
+app.post('/upload', multipart(), function(req, res){
+  //get filename
+
+  var filename = req.files.key.originalFilename || path.basename(req.files.files.ws.path);
+  //copy file to a public directory
+  var targetPath = path.dirname(__filename) + '/public/' + filename;
+  //copy file
+  fs.createReadStream(req.files.files.ws.path).pipe(fs.createWriteStream(targetPath));
+  //return file url
+  res.json({code: 200, msg: {url: 'http://' + req.headers.host + '/' + filename}});
+});
+
+app.listen(3000);
+console.log('Data Lake Portal Site running at: http://0.0.0.0:3000');
